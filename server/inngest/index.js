6370c525235568a9ea038f2bc5026b94d988d6cc
Shekhar Gangwar <shekhargangwar750@gmail.com>
2026-06-17 11:13:48 +0530

    inngest error fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -7,8 +7,8 @@ export const inngest = new Inngest({ id: "pingup-app" });
 
 //Inngest function to save user data to a database
 const syncUserCreation=inngest.createFunction(
-  {id:'sync-user-from-clerk'},
-  {event:'clerk/user.created'},
+  {id:'sync-user-from-clerk',
+  event:'clerk/user.created'},
   async({event})=>{
     const {id,first_name,last_name,email_addresses,image_url}=event.data 
 
@@ -33,8 +33,8 @@ const syncUserCreation=inngest.createFunction(
 )
 //Inngest Function to update user data in database
 const syncUserUpdation=inngest.createFunction(
-  {id:'update-user-from-clerk'},
-  {event:'clerk/user.updated'},
+  {id:'update-user-from-clerk',
+  event:'clerk/user.updated'},
   async({event})=>{
     const {id,first_name,last_name,email_addresses,image_url}=event.data 
 
@@ -49,8 +49,8 @@ const syncUserUpdation=inngest.createFunction(
 )
 //Inngest Function to delete user from database
 const syncUserDeletion=inngest.createFunction(
-  {id:'delete-user-with-clerk'},
-  {event:'clerk/user.updated'},
+  {id:'delete-user-with-clerk',
+  event:'clerk/user.deleted'},
   async({event})=>{
     const {id}=event.data 
     await User.findByIdAndDelete(id)
