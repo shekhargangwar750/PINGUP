f7881a0a47d45368def60436a5754eef897414ce
Shekhar Gangwar <shekhargangwar750@gmail.com>
2026-06-17 13:25:33 +0530

    trigger error solved

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -8,8 +8,13 @@ export const inngest = new Inngest({ id: "pingup-app" });
 //Inngest function to save user data to a database
 const syncUserCreation=inngest.createFunction(
   {id:'sync-user-from-clerk',
-  event:'clerk/user.created'},
+    triggers:[{event:'clerk/user.created'}],
+  },
   async({event})=>{
+      console.log("EVENT RECEIVED");
+  console.log(event);
+
+
     const {id,first_name,last_name,email_addresses,image_url}=event.data 
 
     let username=email_addresses[0].email_address.split('@')[0]
@@ -29,12 +34,19 @@ const syncUserCreation=inngest.createFunction(
       username
     }
     await User.create(userData)
+      console.log("User saved successfully");
+   
+
+
+
+
   }
 )
 //Inngest Function to update user data in database
 const syncUserUpdation=inngest.createFunction(
   {id:'update-user-from-clerk',
-  event:'clerk/user.updated'},
+    triggers:[{event:'clerk/user.updated'}]
+  },
   async({event})=>{
     const {id,first_name,last_name,email_addresses,image_url}=event.data 
 
@@ -50,7 +62,8 @@ const syncUserUpdation=inngest.createFunction(
 //Inngest Function to delete user from database
 const syncUserDeletion=inngest.createFunction(
   {id:'delete-user-with-clerk',
-  event:'clerk/user.deleted'},
+    triggers:[{event:'clerk/user.deleted'}]
+  },
   async({event})=>{
     const {id}=event.data 
     await User.findByIdAndDelete(id)
