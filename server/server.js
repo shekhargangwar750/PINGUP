d7617da096d5c60a6df92ea5b777bc707dde94bb
Shekhar Gangwar <shekhargangwar750@gmail.com>
2026-06-20 10:08:38 +0530

    adding full server

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ import {serve} from 'inngest/express'
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRoutes.js';
 import postRouter from './routes/postRoutes.js';
-// import storyRouter from './routes/storyRoutes.js';
-// import messageRouter from './routes/messageRoutes.js';
+import storyRouter from './routes/storyRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
 
 const app=express();
 await connectDB();
@@ -27,8 +27,8 @@ app.use('/api/inngest',serve({client:inngest,functions}))
 
 app.use('/api/user',userRouter)
 app.use('/api/post',postRouter)
-// app.use('/api/story',storyRouter)
-// app.use('/api/message',messageRouter);
+app.use('/api/story',storyRouter)
+app.use('/api/message',messageRouter);
 
 const PORT=process.env.PORT || 4000;
 
