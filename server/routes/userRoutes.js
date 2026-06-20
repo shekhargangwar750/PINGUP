1772c5745fa42a18b9181c1e099aee235742ce9b
Shekhar Gangwar <shekhargangwar750@gmail.com>
2026-06-20 09:46:55 +0530

    checking userrouter

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,32 +1,35 @@
 import express from 'express'
-import { acceptConnectionRequest, discoverUsers, followUser, getUserConnections, getUserData, sendConnectionRequest, unfollowUser, updateUserData } from '../controllers/userControlller.js';
-import { protect } from '../middlewares/auth.js';
-import { upload } from '../config/multer.js';
-import { getUserProfiles } from '../controllers/postController.js';
-import { getUserRecentMessages } from '../controllers/MessageController.js';
+// import { acceptConnectionRequest, discoverUsers, followUser, getUserConnections, getUserData, sendConnectionRequest, unfollowUser, updateUserData } from '../controllers/userControlller.js';
+// import { protect } from '../middlewares/auth.js';
+// import { upload } from '../config/multer.js';
+// import { getUserProfiles } from '../controllers/postController.js';
+// import { getUserRecentMessages } from '../controllers/MessageController.js';
 const userRouter= express.Router();
 
+userRouter.get('/data',(req,res)=>{
+  res.send('ok')
+})
 
-userRouter.get('/data',protect,getUserData)
+// userRouter.get('/data',protect,getUserData)
 
-userRouter.post('/update',upload.fields([{name:'profile',maxCount:1},{name:'cover',maxCount:1}]),protect,updateUserData)
+// userRouter.post('/update',upload.fields([{name:'profile',maxCount:1},{name:'cover',maxCount:1}]),protect,updateUserData)
 
 
-userRouter.post('/discover',protect,discoverUsers)
+// userRouter.post('/discover',protect,discoverUsers)
 
-userRouter.post('/follow',protect,followUser)
+// userRouter.post('/follow',protect,followUser)
 
-userRouter.post('/unfollow',protect,unfollowUser)
+// userRouter.post('/unfollow',protect,unfollowUser)
 
-userRouter.post('/connect',protect,sendConnectionRequest)
+// userRouter.post('/connect',protect,sendConnectionRequest)
 
-userRouter.post('/accept',protect,acceptConnectionRequest)
+// userRouter.post('/accept',protect,acceptConnectionRequest)
 
-userRouter.get('/connections',protect,getUserConnections)
+// userRouter.get('/connections',protect,getUserConnections)
 
-userRouter.post('/profiles',getUserProfiles)
+// userRouter.post('/profiles',getUserProfiles)
 
-userRouter.get('/recent-messages',protect,getUserRecentMessages)
+// userRouter.get('/recent-messages',protect,getUserRecentMessages)
 
 export default userRouter;
 
