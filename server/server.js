5a51a7b1ccb5cd8118f96f6f9dcd0e4da71f3597
Shekhar Gangwar <shekhargangwar750@gmail.com>
2026-06-20 09:11:16 +0530

    importing inngest file

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 // import connectDB from './config/db.js';
-// import { inngest,functions } from './inngest/index.js';
+import { inngest,functions } from './inngest/index.js';
 // import {serve} from 'inngest/express'
 import { clerkMiddleware } from '@clerk/express'
 // import userRouter from './routes/userRoutes.js';
