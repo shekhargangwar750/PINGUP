8a52d5b4a418273fe37a21bd8e5474241fa7f502
Shekhar Gangwar <shekhargangwar750@gmail.com>
2026-06-20 09:03:44 +0530

    removing all

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 
 
 import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './config/db.js';
+// import cors from 'cors';
+// import 'dotenv/config';
+// import connectDB from './config/db.js';
 // import { inngest,functions } from './inngest/index.js';
 // import {serve} from 'inngest/express'
-import { clerkMiddleware } from '@clerk/express'
-import userRouter from './routes/userRoutes.js';
-import postRouter from './routes/postRoutes.js';
-import storyRouter from './routes/storyRoutes.js';
-import messageRouter from './routes/messageRoutes.js';
+// import { clerkMiddleware } from '@clerk/express'
+// import userRouter from './routes/userRoutes.js';
+// import postRouter from './routes/postRoutes.js';
+// import storyRouter from './routes/storyRoutes.js';
+// import messageRouter from './routes/messageRoutes.js';
 
 const app=express();
-await connectDB();
+// await connectDB();
 
-app.use(express.json());
-app.use(cors());
-app.use(clerkMiddleware())
+// app.use(express.json());
+// app.use(cors());
+// app.use(clerkMiddleware())
 
 
 app.get('/',(req,res)=>{
@@ -25,10 +25,10 @@ app.get('/',(req,res)=>{
 })
 // app.use('/api/inngest',serve({client:inngest,functions}))
 
-app.use('/api/user',userRouter)
-app.use('/api/post',postRouter)
-app.use('/api/story',storyRouter)
-app.use('/api/message',messageRouter);
+// app.use('/api/user',userRouter)
+// app.use('/api/post',postRouter)
+// app.use('/api/story',storyRouter)
+// app.use('/api/message',messageRouter);
 
 const PORT=process.env.PORT || 4000;
 
