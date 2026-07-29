477cf329e765f6498e49133ee7d6583036eb1d83
Shekhar Gangwar <shekhargangwar750@gmail.com>
2026-07-29 18:44:39 +0530

    url changes

--- server/server.js
+++ server/server.js
@@ -1,38 +1,40 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+import userRouter from "./routes/userRoutes.js";
+import postRouter from "./routes/postRoutes.js";
+import storyRouter from "./routes/storyRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
-
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './config/db.js';
-import { inngest,functions } from './inngest/index.js';
-import {serve} from 'inngest/express'
-import { clerkMiddleware } from '@clerk/express'
-import userRouter from './routes/userRoutes.js';
-import postRouter from './routes/postRoutes.js';
-import storyRouter from './routes/storyRoutes.js';
-import messageRouter from './routes/messageRoutes.js';
-
-const app=express();
+const app = express();
 await connectDB();
 
 app.use(express.json());
-app.use(cors());
-app.use(clerkMiddleware())
+app.use(
+  cors({
+    origin: "https://connectx-people.vercel.app",
+    credentials: true,
+  }),
+);
+app.use(clerkMiddleware());
 
-
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   res.send("server is running");
-})
-app.use('/api/inngest',serve({client:inngest,functions}))
+});
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.use('/api/user',userRouter)
-app.use('/api/post',postRouter)
-app.use('/api/story',storyRouter)
-app.use('/api/message',messageRouter);
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+app.use("/api/story", storyRouter);
+app.use("/api/message", messageRouter);
 
-const PORT=process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log("http://localhost:4000");
-  console.log(`server is running on port ${PORT}`)
-})
+  console.log(`server is running on port ${PORT}`);
+});
